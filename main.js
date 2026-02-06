92a1f67d64585bf39307414e046dc66259594a3f
Pen-guOnGithub <bottero.steven@gmail.com>
2026-02-06 17:26:17 -0500

    cake fix attempt #2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import * as THREE from 'three';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
