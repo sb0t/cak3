74580c712f702f705c88fcc69143f9d80a05cf70
Pen-guOnGithub <bottero.steven@gmail.com>
2026-02-06 16:25:32 -0500

    cake

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A cake implemented via ThreeJS.
 
 🔗 Live demo: <https://sb0t.github.io/cak3/>
+
+## Credits
+Sketchfab 3D Model ["2 Layer Cake"](<https://sketchfab.com/3d-models/2-layer-cake-4b67a0b1ef7f4b5c834db9656d43037d>) by [CamVas06](<https://sketchfab.com/CamVas06>) is licensed under [Creative Commons Attribution](<http://creativecommons.org/licenses/by/4.0/>).
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -7,16 +8,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const loader = new GLTFLoader();
+loader.load('./assets/scene.gltf', (gltfScene) => {
+    scene.add(gltfScene.scene);
+});
+
+// CUBE
+/*
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color:0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+*/
 
 camera.position.z = 5;
 
 function animate() {
     cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
